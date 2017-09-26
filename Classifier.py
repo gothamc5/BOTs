80f0136ac9197e663ddcdd0f55f66870da02544c
gothamc5 <gouthamkumar.h@gmail.com>
2017-09-27 00:50:11 +0530

    changeatwork
    added feature creation
    implemented naive bayes alg

--- Classifier.py
+++ Classifier.py
@@ -1,26 +1,32 @@
-# import csv
-# datafile = "./data/Data.csv"
-# with open(datafile, 'r') as f:
-#     reader = csv.reader(f)
-#
-#     your_list = list(reader)
-#
-# print(your_list[0:5])
-
-# import pandas as pd
-# df = pd.from_csv()
-# print(df[:3])
 import nltk
 import pandas as pd
 import numpy as np
-df=pd.read_csv(r".\data\Data2.csv")
-# print(df[-1:])
-train_set = df[:4300]
-test_set = df[4300:]
-classifier = nltk.NaiveBayesClassifier(train_set)
-# df['split'] = np.random.randn(df.shape[0], 1)
-# msk = np.random.rand(len(df)) <= 0.7
-# train = df[msk]
-# print(train)
-# test = df[~msk]
-# print(test)
+from nltk.tokenize import word_tokenize
+
+df=pd.read_csv(r".\data\data1.txt")
+
+mt = df['data'].tolist()
+
+all_words = set()
+for x in mt:
+    for y in x.split():
+        # for y in x.split():
+        all_words.add(y)
+
+def find_features(f):
+    words=set(f.split())
+    features={}
+    for w in all_words:
+        features[w] = (w in words)
+    return features
+
+vdata = df[df.category!='other']
+featuresets = []
+for a in range(vdata.shape[0]):
+    featuresets.append((find_features(vdata.iloc[a][0]),vdata.iloc[a][1]))
+
+train_set = featuresets[:4300]
+test_set = featuresets[4300:]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+print("accuracy: ", nltk.classify.accuracy(classifier,test_set))
