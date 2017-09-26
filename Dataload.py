59c11ac2be5de45d7c531d79d17af44d3244dee1
gothamc5 <gouthamkumar.h@gmail.com>
2017-09-27 00:50:58 +0530

    changeatwork
    small changes

--- Dataload.py
+++ Dataload.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 import re
 import os
+# import BeautifulSoup4
+
 
 datafolder = "./webkb"
 csvdata=open("./data/data2.csv", "w")
-csvdata.write("category,university,data ")
-stopwords = ["and","the","but","for","are","was"]
+csvdata.write("data,category\n")
+stop_words = set(stopwords.words("english"))
 
 for category in os.listdir(datafolder):
     for uni in os.listdir(datafolder+"/"+category):
@@ -17,20 +20,16 @@ for category in os.listdir(datafolder):
                 if "<html>" not in filesrc or "<title>" not in filesrc:
                     continue
                 filehtml=filesrc[filesrc.index("<"):]
-                filetext = BeautifulSoup(filehtml, "html5lib").get_text()
+                filetext = BeautifulSoup(filehtml, "html.parser").get_text()
                 filewords = re.sub('[^a-z| +]', ' ', filetext)
                 fileclean = filewords.split()
-                filedata = [x for x in fileclean if x not in stopwords and len(x)>2]
+                filedata = [x for x in fileclean if x not in stop_words and len(x)>2]
                 filefinal = " ".join(filedata)
                 # csvdata.write(category + "," + uni + "," + filefinal + "\n")
                 csvdata.write( filefinal + "," + category + "\n")
                 f.close()
-            except Exception:
-                print("Unable to read file {0}/{1}/{2}".format(category,uni,files))
+            except Exception as e:
+                print("Unable to process file {0}/{1}/{2}".format(category,uni,files))
+                print(str(e))
                 continue
 csvdata.close()
-
-
-# import collections
-# counter = collections.Counter(fileclean)
-# print(counter)
